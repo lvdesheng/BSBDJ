96c90cb03f49ba687c95b0c79dc654c4034187b1
lvdesheng <lvdesheng123@126.com>
2016-03-06 22:30:56 +0800

    初始化Photos

--- BSBDJ/Classes/Essence(精华)/Controller/LVSeeBigPictureController.m
+++ BSBDJ/Classes/Essence(精华)/Controller/LVSeeBigPictureController.m
@@ -18,6 +18,8 @@
 #import "LVSeeBigPictureController.h"
 #import "LVTopic.h"
 #import "UIImageView+WebCache.h"
+#import "SVProgressHUD.h"
+#import <Photos/Photos.h>
 
 @interface LVSeeBigPictureController ()<UIScrollViewDelegate>
 
@@ -79,8 +81,77 @@
 
 #pragma mark - 按钮点击
 - (IBAction)saveClick:(id)sender {
-    LVFunc
+    
+    //保存图片到相机胶卷 //此方法@selector有固定的格式image:didFinishSavingWithError:contextInfo:
+//    UIImageWriteToSavedPhotosAlbum(self.imageView.image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
+
+    PHAuthorizationStatus oldStatus = [PHPhotoLibrary authorizationStatus];
+    
+    // 判断当前的授权状态
+    [PHPhotoLibrary requestAuthorization:^(PHAuthorizationStatus status) {
+        switch (status) {
+                // 这是系统级别的限制（比如家长控制），用户也无法修改这个授权状态
+            case PHAuthorizationStatusRestricted: {
+                [SVProgressHUD showErrorWithStatus:@"由于系统原因，无法保存图片！"];
+                break;
+            }
+                
+                // 用户已经拒绝当前App访问相片数据（说明用户当初选择了“Don't Allow”）
+            case PHAuthorizationStatusDenied: {
+                if (oldStatus != PHAuthorizationStatusNotDetermined) {
+                    LVLog(@"提醒用户去打开访问开关");
+                }
+                break;
+            }
+                
+                // 用户已经允许当前App访问相片数据（说明用户当初选择了“OK”）
+            case PHAuthorizationStatusAuthorized: {
+                [self saveImage];
+                break;
+            }
+                
+            default:
+                break;
+        }
+    }];
+
 }
+
+
+/**
+ *  保存图片
+ */
+- (void)saveImage
+{
+    // CFStringRef -> Core Foundation (C)
+    // NSString *  -> Foundation (Objective-C)
+    // Core Foundation和Foundation框架的数据类型之间可以利用__bridge互相转换
+    // NSString *string = (__bridge NSString *)kCFBundleNameKey;
+    // CFStringRef string = (__bridge CFStringRef)@"jack";
+    
+    [[PHPhotoLibrary sharedPhotoLibrary] performChanges:^{
+        // 保存图片到【相机胶卷】 - 添加一个新的PHAsset对象
+        [PHAssetChangeRequest creationRequestForAssetFromImage:self.imageView.image];
+        
+        // 从Info.plist中获得App名称
+        NSString *title = [NSBundle mainBundle].infoDictionary[(NSString *)kCFBundleNameKey];
+        // 拥有一个【自定义相册】
+        [PHAssetCollectionChangeRequest creationRequestForAssetCollectionWithTitle:title];
+    } completionHandler:^(BOOL success, NSError * _Nullable error) {
+        LVLog(@"%zd", success);
+    }];
+    
+    // 将保存到【相机胶卷】的图片添加到【自定义相册】
+}
+
+//- (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo
+//{
+//    if (error){
+//        [SVProgressHUD showErrorWithStatus:@"保存失败"];
+//    }else{
+//        [SVProgressHUD showSuccessWithStatus:@"保存成功"];
+//    }
+//}
 
 - (IBAction)back:(id)sender {
     
